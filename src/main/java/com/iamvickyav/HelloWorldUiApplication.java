88507cd638435608774d09c5c5a36e650b0a30e8
iamvickyav <vickyavw.10@gmail.com>
2020-06-23 21:27:27 +0530

    changes for war file deployment

--- src/main/java/com/iamvickyav/HelloWorldUiApplication.java
+++ src/main/java/com/iamvickyav/HelloWorldUiApplication.java
@@ -2,9 +2,16 @@ package com.iamvickyav;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldUiApplication {
+public class HelloWorldUiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelloWorldUiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldUiApplication.class, args);
